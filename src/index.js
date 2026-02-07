61e30ef89e87364966568a2796f2c9dc5ec720ef
Ashish Bhattarai <128586499+calliassan@users.noreply.github.com>
2026-02-07 07:19:37 +0530

    Add logging for MongoDB environment and config
    Log environment and configuration values for MongoDB.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+console.log("ENV MONGODB_URL =", process.env.MONGODB_URL);
+console.log("ENV NODE_ENV =", process.env.NODE_ENV);
+
 const mongoose = require("mongoose");
 const app = require("./app");
 const config = require("./config/config");
 const logger = require("./config/logger"); 
+console.log("CONFIG MONGODB URL =", config.mongoose.url);
+
 mongoose
   .connect(config.mongoose.url, config.mongoose.options)
   .then(() => {
