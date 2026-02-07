96da4d7c3f2e277f2da4e8270f56feb851d1379e
Ashish Bhattarai <128586499+calliassan@users.noreply.github.com>
2026-02-07 08:09:05 +0530

    Update MongoDB connection to encode URI

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ const config = require("./config/config");
 const logger = require("./config/logger"); 
 console.log("CONFIG MONGODB URL =", config.mongoose.url);
 
-mongoose.connect(config.mongoose.url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose
+  .connect(encodeURI(config.mongoose.url), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
     logger.info("Connected to MongoDB");
     app.listen(config.port, () => {
       logger.info(`Listening to port ${config.port}`);
